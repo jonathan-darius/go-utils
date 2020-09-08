e9cc5973dab7327c8d82498609c433666e7dd409
ArpinFidel <richardputra2910@gmail.com>
2020-09-08 16:50:38 +0700

    feat: add redis

---
+++ redis/connect.go
@@ -0,0 +1,55 @@
+package redis
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-redis/redis"
+)
+
+var (
+	redisClient *redis.Client
+)
+
+// ConnectCache func
+func ConnectCache() {
+	connPort := buildRedisEnvString()
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     connPort,
+		Password: os.Getenv("REDIS_PASSWORD"),
+	})
+}
+
+// isCacheConnected func
+// return bool
+func isCacheConnected() bool {
+	if redisClient == nil {
+		ConnectCache()
+	}
+	return redisClient.Ping().Val() == "PONG"
+}
+
+// getRedisClient func
+// return *redis.Client
+func getRedisClient() *redis.Client {
+	if redisClient == nil {
+		ConnectCache()
+	}
+	return redisClient
+}
+
+// buildRedisEnvString func
+// return string
+func buildRedisEnvString() (rtnString string) {
+	if os.Getenv("REDIS_PORT") == "" {
+		os.Setenv("REDIS_PORT", "6379")
+	}
+
+	if os.Getenv("REDIS_HOST") == "" {
+		rtnString = fmt.Sprintf(":%s", os.Getenv("REDIS_PORT"))
+		return
+	}
+
+	rtnString = fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT"))
+	return
+}
