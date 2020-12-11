be8c3c62d52911d8acb499b2b57e613385e69db2
Richard Putra <richardputra2910@gmail.com>
2020-12-11 13:38:41 +0700

    refactor: check banned

--- middleware/auth.go
+++ middleware/auth.go
@@ -3,7 +3,9 @@ package middleware
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/forkyid/go-utils/aes"
@@ -11,9 +13,10 @@ import (
 	"github.com/forkyid/go-utils/jwt"
 	"github.com/forkyid/go-utils/rest"
 	"github.com/go-redis/redis"
+	"github.com/olivere/elastic/v7"
+	"github.com/pkg/errors"
 
 	"github.com/gin-gonic/gin"
-	"github.com/olivere/elastic/v7"
 )
 
 type MemberDataKey struct {
@@ -22,9 +25,9 @@ type MemberDataKey struct {
 }
 
 type MemberData struct {
-	IsBanned   bool       `json:"is_banned,omitempty"`
-	SuspendEnd *time.Time `json:"suspend_end,omitempty"`
-	FCMToken   string     `json:"fcm_token,omitempty"`
+	IsBanned   bool                   `json:"is_banned,omitempty"`
+	SuspendEnd *time.Time             `json:"suspend_end,omitempty"`
+	Data       map[string]interface{} `json:"data,omitempty"`
 }
 
 func (mid *Middleware) Auth(ctx *gin.Context) {
@@ -50,30 +53,28 @@ func (mid *Middleware) Auth(ctx *gin.Context) {
 	}
 
 	if err == redis.Nil {
-		query := elastic.NewMatchQuery("id", aes.Encrypt(id))
-		searchResult, err := mid.elastic.Search().
-			Index("users").
-			Type("_doc").
-			Query(query).
-			Do(context.Background())
-
+		banned, err := isBanned(ctx)
 		if err != nil {
 			rest.ResponseMessage(ctx, http.StatusInternalServerError).
-				Log("auth: ES: " + err.Error())
+				Log("auth: check banned: " + err.Error())
 			ctx.Abort()
 			return
 		}
-
-		if searchResult == nil || searchResult.TotalHits() == 0 {
-			rest.ResponseMessage(ctx, http.StatusNotFound)
-			ctx.Abort()
-			return
+		if banned {
+			status.IsBanned = true
+			err = cache.SetJSON(statusKey, status, 600)
+			if err != nil {
+				rest.ResponseMessage(ctx, http.StatusInternalServerError).
+					Log("auth: cache set: " + err.Error())
+				ctx.Abort()
+				return
+			}
 		}
 
-		err = json.Unmarshal(searchResult.Hits.Hits[0].Source, &status)
+		status, err := getMemberData(mid.elastic, id)
 		if err != nil {
 			rest.ResponseMessage(ctx, http.StatusInternalServerError).
-				Log("auth: unmarshal: " + err.Error())
+				Log("auth: get data: " + err.Error())
 			ctx.Abort()
 			return
 		}
@@ -93,11 +94,57 @@ func (mid *Middleware) Auth(ctx *gin.Context) {
 		return
 	}
 
-	if status.SuspendEnd.After(time.Now()) {
+	if status.SuspendEnd != nil && status.SuspendEnd.After(time.Now()) {
 		rest.ResponseMessage(ctx, http.StatusForbidden, "Suspended")
 		ctx.Abort()
 		return
 	}
 
 	ctx.Next()
+}
+
+func isBanned(ctx *gin.Context) (bool, error) {
+	id, _ := jwt.ExtractID(ctx.GetHeader("Authorization"))
+	query := map[string]string{
+		"block_type_id": aes.Encrypt(1),
+		"blocker_id":    "0",
+		"blocked_id":    aes.Encrypt(id),
+	}
+	req := rest.Request{
+		URL:     fmt.Sprintf("%v/report/v1/blocks", os.Getenv("API_ORIGIN_URL")),
+		Method:  http.MethodGet,
+		Queries: query,
+	}
+	_, code := req.WithContext(ctx).Send()
+	if code == http.StatusNotFound {
+		return false, nil
+	}
+	if code != http.StatusOK {
+		return false, fmt.Errorf("get blocked: status code unexpected: %d", code)
+	}
+	return true, nil
+}
+
+func getMemberData(es *elastic.Client, id int) (status MemberData, err error) {
+	query := elastic.NewMatchQuery("id", aes.Encrypt(id))
+	searchResult, err := es.Search().
+		Index("users").
+		Type("_doc").
+		Query(query).
+		Do(context.Background())
+
+	if err != nil {
+		return status, errors.Wrap(err, "elastic")
+	}
+
+	if searchResult == nil || searchResult.TotalHits() == 0 {
+		return status, errors.Wrap(err, "memebr not found")
+	}
+
+	err = json.Unmarshal(searchResult.Hits.Hits[0].Source, &status)
+	if err != nil {
+		return status, errors.Wrap(err, "unmarshal")
+	}
+
+	return status, nil
 }
