7b4242fc41e7e430fd3b2b0a8313100572ba7df3
ArpinFidel <richardputra2910@gmail.com>
2020-12-11 12:43:19 +0700

    refactor: merge ban, suspend, login refactor: add fcm token

--- middleware/auth.go
+++ middleware/auth.go
@@ -4,44 +4,30 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/forkyid/go-utils/aes"
 	"github.com/forkyid/go-utils/cache"
 	"github.com/forkyid/go-utils/jwt"
 	"github.com/forkyid/go-utils/rest"
+	"github.com/go-redis/redis"
 
 	"github.com/gin-gonic/gin"
 	"github.com/olivere/elastic/v7"
 )
 
-type Status struct {
+type MemberDataKey struct {
 	ID       string `cache:"key"`
 	DeviceID string `cache:"optional" json:"device_id"`
 }
 
-type StatusData struct {
-	Banned    bool
-	Suspended bool
-	LoggedIn  bool
+type MemberData struct {
+	IsBanned   bool       `json:"is_banned,omitempty"`
+	SuspendEnd *time.Time `json:"suspend_end,omitempty"`
+	FCMToken   string     `json:"fcm_token,omitempty"`
 }
 
-type Authorization struct {
-	elastic *elastic.Client
-}
-
-func NewAuthorization(
-	elastic *elastic.Client,
-) *Repository {
-	return &Repository{
-		elastic: elastic,
-	}
-}
-
-type Authorizer interface {
-	Auth(ctx *gin.Context)
-}
-
-func (auth *Authorization) Auth(ctx *gin.Context) {
+func (mid *Middleware) Auth(ctx *gin.Context) {
 	id, err := jwt.ExtractID(ctx.GetHeader("Authorization"))
 	if err != nil {
 		rest.ResponseMessage(ctx, http.StatusUnauthorized)
@@ -49,73 +35,66 @@ func (auth *Authorization) Auth(ctx *gin.Context) {
 		return
 	}
 
-	statusKey := cache.ExternalKey("global", Status{
-		ID:       id,
+	statusKey := cache.ExternalKey("global", MemberDataKey{
+		ID:       aes.Encrypt(id),
 		DeviceID: ctx.GetHeader("X-Unique-ID"),
 	})
 
-	status := StatusData{}
-	exist, err := cache.IsCacheExists(statusKey)
-	if err != nil {
-		rest.ResponseMessage(ctx, http.StatusInternalServerError).Log("auth: redis: check exist: " + err.Error())
+	status := MemberData{}
+	err = cache.GetUnmarshal(statusKey, &status, 600)
+	if err != nil && err != redis.Nil {
+		rest.ResponseMessage(ctx, http.StatusInternalServerError).
+			Log("auth: get unmarshal: " + err.Error())
 		ctx.Abort()
 		return
 	}
-	if !exist {
+
+	if err == redis.Nil {
 		query := elastic.NewMatchQuery("id", aes.Encrypt(id))
-		searchResult, err := esClient.Search().
+		searchResult, err := mid.elastic.Search().
 			Index("users").
 			Type("_doc").
 			Query(query).
 			Do(context.Background())
+
 		if err != nil {
-			rest.ResponseMessage(ctx, http.StatusInternalServerError).Log("auth: ES: " + err.Error())
+			rest.ResponseMessage(ctx, http.StatusInternalServerError).
+				Log("auth: ES: " + err.Error())
 			ctx.Abort()
 			return
 		}
 
 		if searchResult == nil || searchResult.TotalHits() == 0 {
-			rest.ResponseError(ctx, http.StatusNotFound)
+			rest.ResponseMessage(ctx, http.StatusNotFound)
 			ctx.Abort()
 			return
 		}
 
 		err = json.Unmarshal(searchResult.Hits.Hits[0].Source, &status)
 		if err != nil {
-			rest.ResponseMessage(ctx, http.StatusInternalServerError).Log("auth: unmarshal: " + err.Error())
+			rest.ResponseMessage(ctx, http.StatusInternalServerError).
+				Log("auth: unmarshal: " + err.Error())
 			ctx.Abort()
 			return
 		}
 
 		err = cache.SetJSON(statusKey, status, 600)
 		if err != nil {
-			rest.ResponseMessage(ctx, http.StatusInternalServerError).Log("auth: cache set: " + err.Error())
-			ctx.Abort()
-			return
-		}
-	} else {
-		err = cache.GetUnmarshal(statusKey, &status, 600)
-		if err != nil {
-			rest.ResponseMessage(ctx, http.StatusInternalServerError).Log("auth: get unmarshal: " + err.Error())
+			rest.ResponseMessage(ctx, http.StatusInternalServerError).
+				Log("auth: cache set: " + err.Error())
 			ctx.Abort()
 			return
 		}
 	}
 
-	if status.Banned {
+	if status.IsBanned {
 		rest.ResponseMessage(ctx, http.StatusForbidden, "Banned")
 		ctx.Abort()
 		return
 	}
 
-	if status.Suspended {
+	if status.SuspendEnd.After(time.Now()) {
 		rest.ResponseMessage(ctx, http.StatusForbidden, "Suspended")
-		ctx.Abort()
-		return
-	}
-
-	if !status.LoggedIn {
-		rest.ResponseMessage(ctx, http.StatusUnauthorized)
 		ctx.Abort()
 		return
 	}
