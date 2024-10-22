0126def8fe0bd46b7746371127fad8ed0dec5116
Jonathan Darius <jonat496@gmail.com>
2024-10-22 15:10:38 +0700

    feat: implement insrequester

--- v1/nsq/publisher/v1/producer.go
+++ v1/nsq/publisher/v1/producer.go
@@ -1,14 +1,13 @@
 package v1
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"log"
-	"math"
-	"net/http"
 	"os"
 	"time"
+
+	"github.com/useinsider/go-pkg/insrequester"
 
 	connection "github.com/forkyid/go-utils/v1/nsq"
 )
@@ -47,49 +46,21 @@ func pubTypeNSQD(topic string, data []byte) (err error) {
 	return
 }
 
-func backoff(retries int) time.Duration {
-	return time.Duration(math.Pow(2, float64(retries))) * time.Second
-}
-
-func drainBody(resp *http.Response) {
-	if resp.Body != nil {
-		io.Copy(io.Discard, resp.Body)
-		resp.Body.Close()
-	}
-}
-
 func pubTypeHTTPS(topic string, data []byte) (err error) {
 	host := fmt.Sprintf("%s/pub?topic=%s", os.Getenv("NSQD_HOST"), topic)
-	client := &http.Client{}
-	req, err := http.NewRequest(
-		http.MethodPost,
-		host,
-		bytes.NewReader(data),
-	)
-	var resp *http.Response
-	for pubRetry := 0; pubRetry <= retry; pubRetry++ {
-		resp, err = client.Do(req)
-		if err != nil {
-			if pubRetry >= retry {
-				log.Printf("[ERROR] [NSQD] [%s] [%s] %v \n", host, err.Error(), string(data))
-				break
-			}
-			time.Sleep(backoff(pubRetry))
-			continue
-		}
-
-		if resp.StatusCode != http.StatusOK {
-			drainBody(resp)
-			if pubRetry >= retry {
-				log.Printf("[ERROR] [NSQD] [Code: %d] [%s] %v\n", resp.StatusCode, host, string(data))
-				break
-			}
-			time.Sleep(backoff(pubRetry))
-			continue
-		}
-
-		drainBody(resp)
-		break
+	requester := insrequester.NewRequester().
+		WithRetry(insrequester.RetryConfig{
+			WaitBase: 2 * time.Second,
+			Times:    retry,
+		}).Load()
+	resp, err := requester.Post(insrequester.RequestEntity{
+		Endpoint: host,
+		Body:     data,
+	})
+	if err != nil {
+		log.Printf("[ERROR] [NSQD] [%s] [%s] %v \n", host, err.Error(), string(data))
 	}
+	io.Copy(io.Discard, resp.Body)
+	resp.Body.Close()
 	return
 }
