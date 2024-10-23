60933c5b96c754f9bdc25e981d625e93a51fca13
christy <christyalverina@gmail.com>
2024-10-23 10:05:36 +0700

    feat: skip claims validation, move db checking after signature checking

--- v1/jwt/extract.go
+++ v1/jwt/extract.go
@@ -2,20 +2,24 @@ package jwt
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/forkyid/go-utils/v1/aes"
+	"github.com/pkg/errors"
 )
 
 // extractClaims extracts claims from JWT, returns claims as map
 func extractClaims(tokenStr string) (jwt.MapClaims, error) {
 	hmacSecretString := os.Getenv("JWT_ACCESS_SIGNATURE_KEY")
 	hmacSecret := []byte(hmacSecretString)
-	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+
+	parser := new(jwt.Parser)
+	parser.SkipClaimsValidation = true
+
+	token, err := parser.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
 		// check token signing method etc
 		return hmacSecret, nil
 	})
@@ -38,12 +42,14 @@ func ExtractID(ah string) (int, error) {
 	ts := strings.Replace(ah, "Bearer ", "", -1)
 	claimsMap, err := extractClaims(ts)
 	if err != nil {
-		return -1, fmt.Errorf("failed on claiming token")
+		return -1, errors.Wrap(err, "extract claims")
 	}
+
 	id := aes.Decrypt(claimsMap["id"].(string))
 	if id == -1 {
 		return -1, fmt.Errorf("invalid ID")
 	}
+
 	return id, nil
 }
 
@@ -54,17 +60,17 @@ func ExtractClient(ah string) (*AccessClaims, error) {
 
 	claimsMap, err := extractClaims(ts)
 	if err != nil {
-		return nil, fmt.Errorf("failed on claiming token")
+		return nil, errors.Wrap(err, "extract claims")
 	}
 
 	j, err := json.Marshal(&claimsMap)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "marshal")
 	}
 
 	err = json.Unmarshal(j, &claims)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "unmarshal")
 	}
 
 	return &claims, nil
